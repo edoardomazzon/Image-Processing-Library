9203711b07671c241285208db1d35909e4a13b8f
CeccoGab <lele.cec98@gmail.com>
2020-05-04 11:51:05 +0200

    Co-authored-by: edoardomazzon <eduren98@gmail.com>

--- bmp.c
+++ bmp.c
@@ -1,5 +1,5 @@
 /*
- * Bitmap manipulation functions. See `bmp.h` for details. CIAO BRODMMMMMMMM
+ * Bitmap manipulation functions. See `bmp.h` for details.
  */
 #include <stdio.h>
 #include <stdlib.h>
